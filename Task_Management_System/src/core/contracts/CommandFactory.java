d25672b3a7fa03cc308271b1d92076cfa3404290
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-11-13 00:44:37 +0200

    Created the commands package and command interface, in order to be able to start implementig the command factory.

--- Task_Management_System/src/core/contracts/CommandFactory.java
+++ Task_Management_System/src/core/contracts/CommandFactory.java
@@ -1,4 +1,8 @@
 package core.contracts;
 
+import commands.contracts.Command;
+
 public interface CommandFactory {
+
+    Command createCommand(String commandType, BoardRepository boardRepository);
 }
