2bfd167412bff1fcd2e9e52a1a0ee1d92b66cd7b
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-11-20 01:11:48 +0200

    Added a validation for the args count

--- Task_Management_System/src/commands/addCommands/AddPersonToATeamCommand.java
+++ Task_Management_System/src/commands/addCommands/AddPersonToATeamCommand.java
@@ -4,12 +4,15 @@ import Models.Contracts.Person;
 import Models.Contracts.Team;
 import commands.contracts.Command;
 import core.contracts.BoardRepository;
+import util.Validator;
 
 import java.util.List;
 
 public class AddPersonToATeamCommand implements Command {
 
     private static final String PERSON_ADDED_TO_A_TEAM = "%s added to team %s";
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
     private final BoardRepository boardRepository;
 
     public AddPersonToATeamCommand(BoardRepository boardRepository){
@@ -25,6 +28,7 @@ public class AddPersonToATeamCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
+        Validator.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
         String personName = parameters.get(0);
         String teamName = parameters.get(1);
         return AddPersonToATeam(personName, teamName);
