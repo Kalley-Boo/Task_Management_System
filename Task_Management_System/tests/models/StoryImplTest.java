e98e912f93213510b9b47595bdd189e2405b84e2
sbalieva <147823207+sbalieva@users.noreply.github.com>
2023-11-19 21:19:30 +0200

    New tests for StoryImpl.

--- Task_Management_System/tests/models/StoryImplTest.java
+++ Task_Management_System/tests/models/StoryImplTest.java
@@ -1,10 +1,7 @@
 package models;
 
 import Models.CommentImpl;
-import Models.Enums.Priority;
-import Models.Enums.Severity;
-import Models.Enums.TaskSize;
-import Models.Enums.TaskStatus;
+import Models.Enums.*;
 import Models.FeedbackImpl;
 import Models.PersonImpl;
 import Models.StoryImpl;
@@ -21,23 +18,49 @@ public class StoryImplTest {
     public void constructor_Should_ThrowException_When_TitleIsOutOfRange() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new StoryImpl(
-                        5, "Problem", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM,  new PersonImpl("Ivan")));
+                        5, "Problem", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM, new PersonImpl("Petkov")));
     }
+
     @Test
     public void constructor_Should_ThrowException_When_DescriptionIsOutOfRange() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new StoryImpl(
-                        5, "Problem with the system", "Logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM,  new PersonImpl("Ivan")));
+                        5, "Problem with the system", "Logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM, new PersonImpl("Petkov")));
     }
 
     @Test
     public void testEditAssignee() {
         StoryImpl story = new StoryImpl(
-                5, "Problem with the system", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM,  new PersonImpl("Petkov"));
+                5, "Problem with the system", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM, new PersonImpl("Petkov"));
         PersonImpl person = new PersonImpl("Dimo Dimitrov");
         story.editAssignee(person);
         assertEquals(story.getAssignee().getName(), "Dimo Dimitrov");
     }
+    @Test
+    public void testEditStatus(){
+        StoryImpl story = new StoryImpl(
+                5, "Problem with the system", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM, new PersonImpl("Petkov"));
+        story.editStatus(StatusStory.IN_PROGRESS);
+        assertEquals(story.getStatus(), StatusStory.IN_PROGRESS);
+    }
 
+    @Test
+    public void testEditSize(){
+        StoryImpl story = new StoryImpl(
+                5, "Problem with the system", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM, new PersonImpl("Petkov"));
+        story.editSize(TaskSize.LARGE);
+        assertEquals(story.getSize(), TaskSize.LARGE);
+    }
 
+    @Test
+    public void testEditPriority(){
+        StoryImpl story = new StoryImpl(
+                5, "Problem with the system", "Problem when logging", Models.Enums.Priority.HIGH, TaskSize.MEDIUM, new PersonImpl("Petkov"));
+        story.editPriority(Priority.LOW);
+        assertEquals(story.getPriority(), Priority.LOW);
+    }
 }
+
+
+
+
