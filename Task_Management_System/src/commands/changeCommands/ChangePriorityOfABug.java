be1c9bef652bf0617d398a7ef8d6eecc87b91ce2
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-11-20 00:20:31 +0200

    Created a command for changing the bug's priority

---
+++ Task_Management_System/src/commands/changeCommands/ChangePriorityOfABug.java
@@ -0,0 +1,41 @@
+package commands.changeCommands;
+
+import Models.Contracts.Bug;
+import Models.Enums.Priority;
+import Models.Enums.StatusBug;
+import commands.contracts.Command;
+import core.contracts.BoardRepository;
+import util.Parser;
+import util.Validator;
+
+import java.util.List;
+
+public class ChangePriorityOfABug implements Command {
+
+    private static final String PRIORITY_OF_BUG_CHANGED = "The priority of bug '%s' changed %s -> %s.";
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    private final BoardRepository boardRepository;
+
+    public ChangePriorityOfABug(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+
+    private String ChangePriority(String title, Priority priority){
+        Bug bug = boardRepository.findBugByTitle(title);
+        String oldPriority = bug.getPriority().toString();
+        bug.editPriority(priority);
+        return String.format(PRIORITY_OF_BUG_CHANGED, title, oldPriority, priority.toString());
+    }
+
+
+
+    @Override
+    public String execute(List<String> parameters) {
+        Validator.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String title = parameters.get(0);
+        Priority priority = Parser.tryParseEnum(parameters.get(1), Priority.class);
+        return ChangePriority(title, priority);
+    }
+}
