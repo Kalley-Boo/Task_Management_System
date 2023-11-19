9a9f12468d273bde70cbc6ec13e601e14c68c883
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-11-20 00:23:07 +0200

    Created a command for changing the bug's severity

---
+++ Task_Management_System/src/commands/changeCommands/ChangeSeverityOfABug.java
@@ -0,0 +1,41 @@
+package commands.changeCommands;
+
+import Models.Contracts.Bug;
+import Models.Enums.Severity;
+import Models.Enums.StatusBug;
+import commands.contracts.Command;
+import core.contracts.BoardRepository;
+import util.Parser;
+import util.Validator;
+
+import java.util.List;
+
+public class ChangeSeverityOfABug implements Command {
+
+    private static final String SEVERITY_OF_BUG_CHANGED = "The severity of bug '%s' changed %s -> %s.";
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    private final BoardRepository boardRepository;
+
+    public ChangeSeverityOfABug(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+
+    private String ChangeStatus(String title, Severity severity){
+        Bug bug = boardRepository.findBugByTitle(title);
+        String oldSeverity = bug.getSeverity().toString();
+        bug.editSeverity(severity);
+        return String.format(SEVERITY_OF_BUG_CHANGED, title, oldSeverity, severity.toString());
+    }
+
+
+
+    @Override
+    public String execute(List<String> parameters) {
+        Validator.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String title = parameters.get(0);
+        Severity severity = Parser.tryParseEnum(parameters.get(1), Severity.class);
+        return ChangeStatus(title, severity);
+    }
+}
