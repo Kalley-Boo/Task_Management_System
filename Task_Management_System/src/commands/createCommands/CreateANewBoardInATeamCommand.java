b49cfb9ae7f4bf2dd1f70e0c7453934853c404f6
sbalieva <147823207+sbalieva@users.noreply.github.com>
2023-11-20 10:32:10 +0200

    Create A New Board In A Team Command Test

--- Task_Management_System/src/commands/createCommands/CreateANewBoardInATeamCommand.java
+++ Task_Management_System/src/commands/createCommands/CreateANewBoardInATeamCommand.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class CreateANewBoardInATeamCommand implements Command {
 
-    private static final String BOARD_CREATED = "Board with name %s was created in team with name%s!";
+    public static final String BOARD_CREATED = "Board with name %s was created in team with name%s!";
 
     public static final int EXPECTED_PARAMETERS_COUNT = 2;
     private final BoardRepository boardRepository;
