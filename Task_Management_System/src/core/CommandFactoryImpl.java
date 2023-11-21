52c8a72aeebee27e1968cee7129be1f94cdacb5d
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-11-21 02:15:58 +0200

    Ordered the commands in the factory, added what was missing

--- Task_Management_System/src/core/CommandFactoryImpl.java
+++ Task_Management_System/src/core/CommandFactoryImpl.java
@@ -5,6 +5,7 @@ import commands.otherCommands.AddCommentToTaskCommand;
 import commands.changeCommands.*;
 import commands.contracts.Command;
 import commands.enums.CommandType;
+import commands.otherCommands.AddPersonToATeamCommand;
 import commands.otherCommands.AssignTaskToAPersonCommand;
 import commands.otherCommands.UnassignTaskToAPersonCommand;
 import commands.showCommands.*;
@@ -19,50 +20,62 @@ public class CommandFactoryImpl implements CommandFactory {
     public Command createCommand(int commandNumber, BoardRepository boardRepository) {
 
         switch (commandNumber) {
+            //-------------CREATE------------
             case 1:
-                return new CreateNewBugInBoardCommand(boardRepository);
-            case 2:
-                return new CreateNewStoryInBoardCommand(boardRepository);
-            case 3:
-                return new CreateNewFeedbackInBoardCommand(boardRepository);
-            case 4:
-                return new CreateTeamCommand(boardRepository);
-            case 5:
-                return new ShowTeamsActivityCommand(boardRepository);
-            case 6:
                 return new CreatePersonCommand(boardRepository);
-            case 7:
+            case 2:
                 return new CreateBoardCommand(boardRepository);
-            case 8:
-                return new ShowAllPeopleCommand(boardRepository);
-            case 9:
-                return new ShowAllTeamsCommand(boardRepository);
-            case 10:
-                return new ShowPersonActivityCommand(boardRepository);
-            case 11:
-                return new AddCommentToTaskCommand(boardRepository);
-            case 12:
-                return new ShowTeamMembersCommand(boardRepository);
-            case 13:
-                return new ChangeStoryPriorityCommand(boardRepository);
-            case 14:
-                return new ChangeStorySizeCommand(boardRepository);
-            case 15:
+            case 3:
+                return new CreateNewBugInBoardCommand(boardRepository);
+            case 4:
+                return new CreateNewStoryInBoardCommand(boardRepository);
+            case 5:
+                return new CreateNewFeedbackInBoardCommand(boardRepository);
+            case 6:
+                return new CreateTeamCommand(boardRepository);
+            case 7:
                 return new CreateANewBoardInATeamCommand(boardRepository);
-            case 16:
-                return new ShowAllTeamBoardsCommand(boardRepository);
-            case 17:
-                return new ShowBoardSActivityCommand(boardRepository);
-            case 18:
-                return new ChangeRatingOfAFeedbackCommand(boardRepository);
-            case 19:
-                return new ChangeStatusOfAFeedbackCommand(boardRepository);
-            case 20:
-                return new AssignTaskToAPersonCommand(boardRepository);
-            case 21:
-                return new UnassignTaskToAPersonCommand(boardRepository);
-            case 22:
+            //-------------CHANGE-----------
+            case 8:
+                return new ChangeStoryPriorityCommand(boardRepository);
+            case 9:
+                return new ChangeStorySizeCommand(boardRepository);
+            case 10:
                 return new ChangeStoryStatusCommand(boardRepository);
+            case 11:
+                return new ChangeRatingOfAFeedbackCommand(boardRepository);
+            case 12:
+                return new ChangeStatusOfAFeedbackCommand(boardRepository);
+            case 13:
+                return new ChangePriorityOfABug(boardRepository);
+            case 14:
+                return new ChangeSeverityOfABug(boardRepository);
+            case 15:
+                return new ChangeStatusOfABug(boardRepository);
+                //------------OTHERS------------
+            case 16:
+                return new AddCommentToTaskCommand(boardRepository);
+            case 17:
+                return new AddPersonToATeamCommand(boardRepository);
+            case 18:
+                return new AssignTaskToAPersonCommand(boardRepository);
+            case 19:
+                return new UnassignTaskToAPersonCommand(boardRepository);
+                //------------SHOW---------------
+            case 20:
+                return new ShowTeamsActivityCommand(boardRepository);
+            case 21:
+                return new ShowAllPeopleCommand(boardRepository);
+            case 22:
+                return new ShowAllTeamsCommand(boardRepository);
+            case 23:
+                return new ShowPersonActivityCommand(boardRepository);
+            case 24:
+                return new ShowTeamMembersCommand(boardRepository);
+            case 25:
+                return new ShowAllTeamBoardsCommand(boardRepository);
+            case 26:
+                return new ShowBoardSActivityCommand(boardRepository);
             default:
 
                 throw new UnsupportedOperationException(String.format(COMMAND_NOT_SUPPORTED_MESSAGE, commandNumber));
