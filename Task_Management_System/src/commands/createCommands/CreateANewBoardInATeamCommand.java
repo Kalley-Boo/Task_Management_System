bb96c948c198a31b9ac500a1fed2241fb878a485
sbalieva <147823207+sbalieva@users.noreply.github.com>
2023-11-22 11:13:24 +0200

    Create New Board In a Team Validation

--- Task_Management_System/src/commands/createCommands/CreateANewBoardInATeamCommand.java
+++ Task_Management_System/src/commands/createCommands/CreateANewBoardInATeamCommand.java
@@ -1,7 +1,10 @@
 package commands.createCommands;
 
+import Models.BoardImpl;
+import Models.Contracts.Board;
 import commands.contracts.Command;
 import core.contracts.BoardRepository;
+import exceptions.InvalidInputException;
 import util.Validator;
 
 import java.util.ArrayList;
@@ -13,6 +16,7 @@ public class CreateANewBoardInATeamCommand implements Command {
     public static final String INVALID_TITLE_LENGTH = "The length of the title must be 5-10";
 
     public static final int EXPECTED_PARAMETERS_COUNT = 2;
+    public static final String BOARD_EXISTS = "Board with the same name already exists.";
     private final BoardRepository boardRepository;
 
 
@@ -22,7 +26,6 @@ public class CreateANewBoardInATeamCommand implements Command {
         expectedArguments.add("a title for the board (5-10 characters)");
         expectedArguments.add("a team to which it will be assigned");
     }
-
     @Override
     public String execute(List<String> parameters) {
         Validator.validateArgumentsCount(parameters, EXPECTED_PARAMETERS_COUNT);
@@ -32,14 +35,24 @@ public class CreateANewBoardInATeamCommand implements Command {
         //todo validate if the board already exists in the team
         return createANewBoardInATeam(boardName, teamName);
     }
-
     @Override
     public List<String> getExpectedArguments() {
         return expectedArguments;
     }
+    private boolean boardExists(String boardName, String teamName){
+       int number = boardRepository.getTeams().indexOf(boardName);
+        for (Board bord: boardRepository.findTeamByName(teamName).getBoards()
+             ) {if (bord.getName().equals(boardName)){return true;}
+
+        } return false;
+    }
 
     private String createANewBoardInATeam(String boardName, String teamName) {
+        if (boardExists(boardName, teamName)){
+            throw new InvalidInputException(BOARD_EXISTS);
+        }
         boardRepository.createANewBoardInATeam(boardName, teamName);
         return String.format(BOARD_CREATED, boardName, teamName);
     }
+
 }
