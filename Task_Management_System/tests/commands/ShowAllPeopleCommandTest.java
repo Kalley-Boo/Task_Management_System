7b8f71cf30f027957822cfa9d035a359d254b3fe
Ivaylo37 <ivaylo.vassilev99@gmail.com>
2023-11-24 15:46:55 +0200

    finished the tests for show all people command

--- Task_Management_System/tests/commands/ShowAllPeopleCommandTest.java
+++ Task_Management_System/tests/commands/ShowAllPeopleCommandTest.java
@@ -7,22 +7,46 @@ import core.BoardRepositoryImpl;
 import core.contracts.BoardRepository;
 //import org.testng.annotations.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShowAllPeopleCommandTest {
+    private static final String ALL_PEOPLE_HEADER = "---All people with their tasks---";
+    private static final String NO_PEOPLE = "There are no people.";
 
-    //@Test
+    BoardRepository boardRepository;
+    @BeforeEach
+    public void setUp(){
+        this.boardRepository = new BoardRepositoryImpl();
+    }
+    @Test
     public void executeShouldReturnThePersonInTheRepository(){
-        BoardRepository boardRepository = new BoardRepositoryImpl();
         Command create = new CreatePersonCommand(boardRepository);
         List<String> args = new ArrayList<>();
         args.add("aaaaa");
         create.execute(args);
         Command show = new ShowAllPeopleCommand(boardRepository);
-        Assertions.assertEquals(boardRepository.getPeople().get(0).toString(), show.execute(new ArrayList<>()));
-        //TODO rework when person is printable
+        StringBuilder expected = new StringBuilder();
+        expected.append(ALL_PEOPLE_HEADER).append("\n");
+        expected.append(boardRepository.findPersonByName(args.get(0)).print());
+        expected.append("\n");
+        expected.append(ALL_PEOPLE_HEADER);
+        Assertions.assertEquals(new String(expected), show.execute(new ArrayList<>()));
 
+    }
+
+    @Test
+    public void executeShouldReturnMessageWhenThereAreNoPeople(){
+        Command show = new ShowAllPeopleCommand(boardRepository);
+        Assertions.assertEquals(NO_PEOPLE, show.execute(new ArrayList<>()));
+    }
+
+    @Test
+    public void getArguments_should_return_a_list(){
+        Command command = new ShowAllPeopleCommand(boardRepository);
+        Assertions.assertEquals(command.getExpectedArguments().size(), 0);
     }
 }
