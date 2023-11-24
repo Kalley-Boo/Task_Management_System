b4266119cb543a474b6aebccbd49e23f765f7052
sbalieva <147823207+sbalieva@users.noreply.github.com>
2023-11-24 12:36:03 +0200

    Filter Story By Assignee Command

--- Task_Management_System/src/commands/showCommands/FilterBugByAssigneeCommand.java
+++ Task_Management_System/src/commands/showCommands/FilterBugByAssigneeCommand.java
@@ -15,8 +15,8 @@ public class FilterBugByAssigneeCommand implements Command {
 
     private final BoardRepository boardRepository;
     private final List<String> expectedArguments;
-    public static final String TITLE = "Enter assignee (will show all tasks that contain this assignee.)";
-    public static final String NO_TASKS_FOUND = "No tasks with this assignee were found";
+    public static final String TITLE = "Enter assignee (will show all bugs that contain this assignee.)";
+    public static final String NO_TASKS_FOUND = "No bugs with this assignee were found";
     private static final int EXPECTED_PARAMETERS_COUNT = 1;
 
     public FilterBugByAssigneeCommand(BoardRepository boardRepository) {
@@ -24,7 +24,7 @@ public class FilterBugByAssigneeCommand implements Command {
         this.expectedArguments = new ArrayList<>();
         expectedArguments.add(TITLE);
     }
-    private String filterTasks(String sentence) {
+    private String filterBugs(String sentence) {
         List<Bug> filteredTasks = boardRepository.getBugs().stream().filter(bug -> bug.getAssignee().getName().equals(sentence)).collect(Collectors.toList());
         if (filteredTasks.isEmpty()) {
             return NO_TASKS_FOUND;
@@ -39,7 +39,7 @@ public class FilterBugByAssigneeCommand implements Command {
     @Override
     public String execute(List<String> parameters) {
         Validator.validateArgumentsCount(parameters, EXPECTED_PARAMETERS_COUNT);
-        return filterTasks(parameters.get(0));
+        return filterBugs(parameters.get(0));
     }
 
     @Override
---
+++ Task_Management_System/src/commands/showCommands/FilterStoryByAssigneeCommand.java
@@ -0,0 +1,45 @@
+package commands.showCommands;
+
+import Models.Contracts.Bug;
+import Models.Contracts.Story;
+import commands.contracts.Command;
+import core.contracts.BoardRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterStoryByAssigneeCommand implements Command {
+    private final BoardRepository boardRepository;
+    private final List<String> expectedArguments;
+    public static final String TITLE = "Enter assignee (will show all stories that contain this assignee.)";
+    public static final String NO_TASKS_FOUND = "No stories with this assignee were found";
+    private static final int EXPECTED_PARAMETERS_COUNT = 1;
+
+    public FilterStoryByAssigneeCommand(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+        this.expectedArguments = new ArrayList<>();
+        expectedArguments.add(TITLE);
+    }
+
+    private String filterStories(String sentence) {
+        List<Story> filteredTasks = boardRepository.getStories().stream().filter(story -> story.getAssignee().getName().equals(sentence)).collect(Collectors.toList());
+        if (filteredTasks.isEmpty()) {
+            return NO_TASKS_FOUND;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Story story : filteredTasks) {
+            stringBuilder.append(story.getTitle()).append("\n");
+        }
+        return new String(stringBuilder).trim();
+    }
+    @Override
+    public String execute(List<String> parameters) {
+        return null;
+    }
+
+    @Override
+    public List<String> getExpectedArguments() {
+        return null;
+    }
+}
