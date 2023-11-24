caae90fa71a0c3485ff58c59c7136659b7164f3f
kalina <borissova.k@icloud.com>
2023-11-24 15:33:58 +0200

    fixing the SortBugByStatus command again

--- Task_Management_System/src/commands/showCommands/FilterBugsbyStatus.java
+++ Task_Management_System/src/commands/showCommands/FilterBugsbyStatus.java
@@ -1,33 +1,51 @@
 package commands.showCommands;
 
+import Models.Contracts.Bug;
 import commands.contracts.Command;
 import core.contracts.BoardRepository;
 import util.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FilterBugsbyStatus implements Command {
     public static final String TITLE = "Enter status (will show all bugs that are with this status)";
     public static final String NO_BUGS = "There are no bugs currently.";
     private static final int EXPECTED_PARAMETERS_COUNT = 1;
+    private final List<String> expectedArguments;
     private final BoardRepository boardRepository;
 
     public FilterBugsbyStatus(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
+        this.expectedArguments = new ArrayList<>();
+        expectedArguments.add(TITLE);
     }
 
-    private String sortBugsByStatus(String status){
+    private String filterBugsByStatus(String status){
+        List<Bug> filteredBugs = boardRepository.getBugs()
+                .stream()
+                .filter(bug -> bug.getTaskStatus().equals(status))
+                .toList();
+        if(filteredBugs.isEmpty()){
+            return NO_BUGS;
+        }
 
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Bug bug : filteredBugs){
+            stringBuilder.append(bug.getTitle()).append("\n");
+            stringBuilder.append(bug.getTaskStatus());
+        }
+        return new String(stringBuilder);
     }
 
     @Override
     public String execute(List<String> parameters) {
         Validator.validateArgumentsCount(parameters, EXPECTED_PARAMETERS_COUNT);
-        return sortBugsByStatus(parameters.get(0));
+        return filterBugsByStatus(parameters.get(0));
     }
 
     @Override
     public List<String> getExpectedArguments() {
-        return expectedArguments();
+        return expectedArguments;
     }
 }
