db62e4cc29b75a2e701c06ab04a66c616d93be81
kalina <borissova.k@icloud.com>
2023-11-28 12:10:34 +0200

    Adding sort bugs by priority test

---
+++ Task_Management_System/tests/filteringAndSorting/SortBugsByPriorityTest.java
@@ -0,0 +1,52 @@
+package filteringAndSorting;
+
+import commands.FilteringAndSorting.SortBugsByPriority;
+import core.BoardRepositoryImpl;
+import core.contracts.BoardRepository;
+import models.PersonImpl;
+import models.contracts.Person;
+import models.enums.Priority;
+import models.enums.Severity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortBugsByPriorityTest {
+    private BoardRepository boardRepository;
+    private SortBugsByPriority sortBugsByPriority;
+
+    @BeforeEach
+    public void setUp() {
+        this.boardRepository = new BoardRepositoryImpl();
+        this.sortBugsByPriority = new SortBugsByPriority(boardRepository);
+    }
+    @Test
+    public void testSortBugsByPriority(){
+        ArrayList<String> parameters = new ArrayList<>();
+        parameters.add("Hello");
+        Person person1 = new PersonImpl("IvanOne");
+        Person person2 = new PersonImpl("IvanIvan");
+        Person person3 = new PersonImpl("IvanOne");
+
+        boardRepository.createAssignedBug("BugTestOne",
+                "Ajshdadjhfabdfk", parameters,
+                Priority.HIGH, Severity.MINOR, person1);
+        boardRepository.createAssignedBug("BugTestTwo",
+                "Ajshdadjhfabdfk", parameters,
+                Priority.LOW, Severity.MINOR, person2);
+        boardRepository.createAssignedBug("BugTestThree",
+                "Ajshdadjhfabdfk", parameters,
+                Priority.MEDIUM, Severity.MINOR, person3);
+        String result = sortBugsByPriority.execute(new ArrayList<>());
+        assertEquals("Bug's title: BugTestOne\n" +
+                "The bug's priority: HIGH\n" +
+                "Bug's title: BugTestThree\n" +
+                "The bug's priority: MEDIUM\n" +
+                "Bug's title: BugTestTwo\n" +
+                "The bug's priority: LOW", result.trim());
+    }
+
+}
